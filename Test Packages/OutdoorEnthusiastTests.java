fbc7716b76bc552aa7639cd50cb34083781b49a9
virginiam1203 <VirginiaM1203@gmail.com>
2015-03-02 12:14:34 -0500

    Added second scenario for skiing to outdoor enthusiast

--- Test Packages/OutdoorEnthusiastTests.java
+++ Test Packages/OutdoorEnthusiastTests.java
@@ -19,7 +19,8 @@ public class OutdoorEnthusiastTests  {
   @Before
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    //wait 60 seconds for elements to appear/pages to load
+    driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
   }
 // Search using keyword through Google search
 
@@ -45,7 +46,30 @@ public class OutdoorEnthusiastTests  {
         //Assert that we are at the golf forecast page
         assert(driver.getTitle().contains("Golf Weather Forecast"));
   }
-
+  
+  /*
+   * Scenario: Check the ski forecast
+   * Given an accuweather home page without ski as an interest tab
+   * When the user attempts to add a ski tab
+   * Then the ski tab should be added to interests
+   * And the user should be able to click it to navigate to the ski forecast
+   */
+  @Test
+  public void CheckSki() throws Exception {
+        //Open Home Page
+        driver.get("http://www.accuweather.com");
+        //Find the add ski to interests button in dropdown menu
+        WebElement addNewInterest = driver.findElement(By.id("add-new-interest"));
+        Actions builder = new Actions(driver);
+        builder.moveToElement( addNewInterest );    
+        builder.perform();   
+        driver.findElement(By.id("add-ski")).click();
+        //Click on the golf button
+        driver.findElement(By.cssSelector("a[href*='ski-weather']")).click();
+        //Assert that we are at the golf forecast page
+        assert(driver.getTitle().contains("Skiing Weather Forecast"));
+  }
+  
   @After
   public void tearDown() throws Exception {
     driver.quit();
